e7298c988625c0f620c90bb3819416d5e76c32a1
Ian Cardoso <ian.cardoso@zup.com.br>
2021-10-26 12:29:04 -0300

    language_detect: improve tests, file matching checks and to_ignore paths
    this commits updates language detect tests and some file matching checks now using filepath.Join(), removing ReplacePathSeparator usages on this package as discussed in #719 and fix to_ignore paths to run as it should
    
    Signed-off-by: Ian Cardoso <ian.cardoso@zup.com.br>

--- e2e/commands/generate/generate_test.go
+++ e2e/commands/generate/generate_test.go
@@ -16,13 +16,14 @@ package generate_test
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
-	"os"
-	"path/filepath"
-	"strings"
 
 	"github.com/ZupIT/horusec/internal/utils/testutil"
 )
